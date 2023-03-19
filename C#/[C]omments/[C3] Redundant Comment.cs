3ba6556138ef4b719af06e6f8402a24dab3d0473
Serhii Chepets <chepets9685@gmail.com>
2023-03-19 16:34:17 +0200

    Change example.

--- C#/[C]omments/[C3] Redundant Comment.cs
+++ C#/[C]omments/[C3] Redundant Comment.cs
@@ -1,10 +1,13 @@
 // This is an example method with a redundant comment.
 // The comment repeats what the method name already says.
 
-/**
- * This method returns the sum of two numbers.
- */
-public int CalculateSum(int a, int b)
+/// <summary>
+/// This method returns the sum of two numbers.
+/// </summary>
+/// <param name="sellRequest">Sell request</param>
+/// <returns>Sell response</returns>
+public SellResponse BeginSellItem(SellRequest sellRequest)
 {
     // Code implementation here...
+    // throw new ManagedComponentException();
 }
