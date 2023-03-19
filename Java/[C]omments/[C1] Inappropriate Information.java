48a670e1c25de97f19268d8b0e022d804e7d89f9
Serhii Chepets <chepets9685@gmail.com>
2023-03-19 16:36:34 +0200

    Update example.

--- Java/[C]omments/[C1] Inappropriate Information.java
+++ Java/[C]omments/[C1] Inappropriate Information.java
@@ -1,3 +1,6 @@
+// This is an example class that violates the rule of including meta-data in comments.
+// It contains information about the author and last modified date.
+
 /**
 Author: John Doe
 Last modified: 1/1/2022
