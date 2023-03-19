3dd07f383385b25a19749fc8a9b1c28e2c2fb447
Serhii Chepets <chepets9685@gmail.com>
2023-03-19 16:34:58 +0200

    Change example.

--- Java/[C]omments/[C3] Redundant Comment.java
+++ Java/[C]omments/[C3] Redundant Comment.java
@@ -1,7 +1,13 @@
 // This is an example method with a redundant comment.
 // The comment repeats what the method name already says.
 
-public int calculateSum(int a, int b) {
+/**
+This method performs a calculation on two integers and returns the result.
+@param a the first integer for the calculation
+@param b the second integer for the calculation
+@return the result of the calculation
+@throws Exception if the input integers are invalid
+*/
+public int doCalculation(int a, int b) throws Exception {
 // Code implementation here...
-// No need for additional comments as the method name is self-explanatory.
 }
