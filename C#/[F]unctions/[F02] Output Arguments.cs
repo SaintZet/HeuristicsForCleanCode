c8e9f56aa21662c9a9f1b8e8ee828e47dfa6bef7
Inna <33370054+InnaMel@users.noreply.github.com>
2024-02-21 15:14:40 +0200

    minor fix (#12)
    It changes the Java class StringBuffer to the C# class StringBuilder

--- C#/[F]unctions/[F02] Output Arguments.cs
+++ C#/[F]unctions/[F02] Output Arguments.cs
@@ -3,7 +3,7 @@ AppendFooter(s);
 // Does this function append s as the footer to something?
 //  Or does it append some footer to s? Is s an input or an output?
 //  It doesn’t take long to look at the function signature and see:
-public void AppendFooter(StringBuffer report)
+public void AppendFooter(StringBuilder report)
 
 // This clarifies the issue, but only at the expense of checking the declaration of the function.
 // Anything that forces you to check the function signature is equivalent to a double-take.
